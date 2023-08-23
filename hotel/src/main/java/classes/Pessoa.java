8fb97e35121f6e5b8076eb4c1f3fe39b6f33dd9a
Patrick Vinicius Coelho Lima <PET0152991@aluno.firjansenaisesi.com.br>
2023-08-23 11:02:56 -0300

    Create pessoa class and deleting funcionario and cliente class

---
+++ hotel/src/main/java/classes/Pessoa.java
@@ -0,0 +1,19 @@
+package classes;
+
+public class Pessoa {
+    public String nome;
+    public String email;
+    private String cpf;
+
+    
+
+
+
+
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
